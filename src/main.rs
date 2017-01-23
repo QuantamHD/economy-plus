e5bac0d865bc0ba46b89fbd806b4d8fd81694999
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-01-23 12:13:33 -0800

    Add Mustache in folder names

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use mustache::MapBuilder;
 use walkdir::WalkDir;
 use std::path::Path;
 use std::fs;
+use std::io::BufWriter;
+use std::str;
 
 /// Compiles a mustache file and then spits out the output to a file
 ///
@@ -19,11 +21,11 @@ use std::fs;
 /// ```
 /// compile_mustache_file(Path::new("input.txt"), Path::new("output.txt");)
 /// ```
-fn compile_mustache_file(input : &Path, output : &Path) {
+fn compile_mustache_file(input : &Path, output : &Path, name : &String) {
     let template = mustache::compile_path(input).unwrap();
 
     let data = MapBuilder::new()
-        .insert_str("name", "Evan")
+        .insert_str("name", name)
         .build();
 
     let mut buffer = File::create(output).unwrap();
@@ -34,18 +36,31 @@ fn compile_mustache_file(input : &Path, output : &Path) {
 /// the container folder.
 ///
 /// # Examples
-/// 
+///
 /// ```
 /// compile_folder(Path::new("example_project"), Path::new("output_project"));
 /// ```
-fn compile_folder(input_folder : &Path, output_container_folder : &Path) {
+fn compile_folder(input_folder : &Path, output_container_folder : &Path, name : &String) {
     for entry in WalkDir::new(input_folder) {
         let entry = entry.unwrap();
         if !entry.path().is_dir() { // If it's a file, let's compile
             let input = entry.path().as_os_str();
-            compile_mustache_file(entry.path(), output_container_folder.join(input).as_path());
+            compile_mustache_file(entry.path(),
+                output_container_folder.join(input).as_path(),
+                name);
         } else { // If a directory, create that directory
-            fs::create_dir_all(output_container_folder.join(entry.path()));
+            let path_name = output_container_folder.join(entry.path());
+            let mustached_folder : &str = path_name.to_str().unwrap();
+
+            let template = mustache::compile_str(mustached_folder).unwrap();
+            let data = MapBuilder::new()
+                .insert_str("name", name)
+                .build();
+
+            let mut buffer : Vec<u8> = Vec::new();
+            template.render_data(&mut buffer, &data).unwrap();
+
+            fs::create_dir_all(Path::new(&String::from_utf8(buffer).unwrap()));
         }
     }
 }
@@ -62,6 +77,5 @@ fn main() {
 
     println!("The name of the command {}", name);
 
-
-    compile_folder(Path::new("example_project"), Path::new("output_project"));
+    compile_folder(Path::new("example_project"), Path::new("output_project"), &name.to_string());
 }
