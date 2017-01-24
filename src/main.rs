3a695485cb8e1a68c6d33709fd8152e72595a826
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-01-23 16:47:39 -0800

    Moved compile_folder into compile.rs

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,12 @@
-extern crate walkdir;
 extern crate mustache;
 mod templates;
 
 #[macro_use]
 extern crate clap;
 
-use walkdir::WalkDir;
 use std::path::Path;
-use std::fs;
 use std::str;
-use templates::mustache_template::compile::*;
-
-/// Compiles every file in mustache and outputs the result as a folder inside
-/// the container folder.
-///
-/// # Examples
-///
-/// ```
-/// compile_folder(Path::new("example_project"), Path::new("output_project"));
-/// ```
-fn compile_folder(input_folder : &Path, output_container_folder : &Path, name : &String) {
-    for entry in WalkDir::new(input_folder) {
-        let entry = entry.unwrap();
-
-        if !entry.path().is_dir() {
-
-            // If it's a file, let's compile
-            let input = entry.path().as_os_str();
-            compile_to_file(entry.path(),
-                output_container_folder.join(input).as_path(),
-                name);
-        } else {
-
-            // If a directory, create that directory
-            let path_name = output_container_folder.join(entry.path());
-            let mustached_folder : &str = path_name.to_str().unwrap();
-            let compiled_path_name = compile_string(&String::from(mustached_folder), name);
-            fs::create_dir_all(Path::new(&compiled_path_name)); // TODO Error check
-        }
-    }
-}
+use templates::mustache_template::compile::compile_folder;
 
 fn main() {
     let matches = clap_app!(myapp =>
--- src/templates/mustache_template/compile.rs
+++ src/templates/mustache_template/compile.rs
@@ -1,9 +1,14 @@
 extern crate mustache;
+extern crate walkdir;
+
 
 use std::fs::File;
 use self::mustache::MapBuilder;
 use std::path::Path;
 use std::result::Result;
+use self::walkdir::WalkDir;
+use std::fs;
+
 
 
 /// Compiles a mustache file and then spits out the output to a file
@@ -43,4 +48,34 @@ pub fn compile_string(input : &String, name: &String) -> String {
     template.render_data(&mut buffer, &data).unwrap();
 
     String::from_utf8(buffer).unwrap()
+}
+
+/// Compiles every file in mustache and outputs the result as a folder inside
+/// the container folder.
+///
+/// # Examples
+///
+/// ```
+/// compile_folder(Path::new("example_project"), Path::new("output_project"));
+/// ```
+pub fn compile_folder(input_folder : &Path, output_container_folder : &Path, name : &String) {
+    for entry in WalkDir::new(input_folder) {
+        let entry = entry.unwrap();
+
+        if !entry.path().is_dir() {
+
+            // If it's a file, let's compile
+            let input = entry.path().as_os_str();
+            compile_to_file(entry.path(),
+                output_container_folder.join(input).as_path(),
+                name);
+        } else {
+
+            // If a directory, create that directory
+            let path_name = output_container_folder.join(entry.path());
+            let mustached_folder : &str = path_name.to_str().unwrap();
+            let compiled_path_name = compile_string(&String::from(mustached_folder), name);
+            fs::create_dir_all(Path::new(&compiled_path_name)); // TODO Error check
+        }
+    }
 }
